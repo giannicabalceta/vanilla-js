abcc5adfbde0ee7ecfbcb3a49f31bf161115490c
giannicabalceta <gcabalceta@fwdcostarica.com>
2024-08-16 11:31:06 -0600

    push

--- src/ejercicio1.js
+++ src/ejercicio1.js
@@ -1,8 +1,13 @@
 let contador = {
   valor: 0,
   siguiente: function () {
-    // Inserte el código aquí
+   return this.valor++
+   
   },
+
+
 };
+
+
 
 export { contador }
--- src/ejercicio2.js
+++ src/ejercicio2.js
@@ -1,7 +1,10 @@
 class Contador {
   // Inserte el código aquí
+  valor= 0;
+
 }
 
 // Inserte el código aquí
+
 
 export { instancia }
